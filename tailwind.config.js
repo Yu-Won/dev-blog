cec9c86e01b70e3b6c419278988256fc93e3e246
Yu-Won <shallwedance0419@gmail.com>
2022-08-26 15:58:28 +0900

    add: responsive width

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "320px",
+        tablet: "768px",
+        laptop: "1024px",
+        desktop: "2560px",
+      }
+    }
   },
   plugins: [],
 }

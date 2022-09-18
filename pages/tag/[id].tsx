417f8ae40b386ece3dfd9b7bd0d270993468ba26
Yu-Won <shallwedance0419@gmail.com>
2022-09-18 16:42:48 +0900

    add: tag page and ListLayout component

--- pages/tag/[id].tsx
+++ pages/tag/[id].tsx
@@ -1,36 +1,50 @@
 import { GetStaticProps } from "next";
-import { getAllTagsFromPosts } from "utils/mdxUtils";
+import { getAllPosts, getAllTagsFromPosts } from "utils/mdxUtils";
 import Title from "components/Title";
+import ListLayout from "components/ListLayout";
+import { Post } from "types";
 
 interface ITagProps {
-    [key: string]: string | undefined;
+    [key: string]: string | undefined | Post[];
     id: string;
+    posts: Post[];
 }
 
+
 const Tag = (props: ITagProps) => {
+    const { id, posts } = props;
     return (
         <div className="flex flex-col w-full">
-            <Title id={props.id} />
+            <Title id={id} />
+            <ListLayout  posts={posts}/>
         </div>
     )
 };
 
 export const getStaticPaths = async () => {
     const allTags = await getAllTagsFromPosts();
+    const posts = await getAllPosts();
+
+    const paths: { params: { id: string; } }[] = [];
+    allTags.forEach(({ tag }) => {
+        paths.push({ params: { id: tag } })
+    });
+
     return {
-        paths: allTags.map((tags) => {
-            return { params: { id: tags.tag } }}
-        ),
+        paths,
         fallback: "blocking"
     }
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const allPosts = await getAllPosts();
     const { id } = params as ITagProps;
+    const resultPosts = allPosts.filter((post) => post.frontMatter.tags.find((t) => t === id))
     return {
         props: {
-            id
-        }
+            posts: resultPosts.map((post) => ({ ...post, path: '' })),
+            id,
+        },
     }
 };
 

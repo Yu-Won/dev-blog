e05f602602f2ccba6aa604b4f1d962c4139133ec
YU-WON <shallwedance0419@gmail.com>
2022-11-21 21:44:53 +0900

    modify: logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+import Lottie from "lottie-react";
 import { useTheme } from "next-themes";
+import cat from "../public/json/indigo-cat.json";
 import { MoonIcon } from "@heroicons/react/24/solid";
 import { SunIcon } from "@heroicons/react/24/outline";
 
@@ -11,21 +13,21 @@ const Header = () => {
     setMounted(true);
   }, []);
 
-  if(!mounted) return null;
+  if (!mounted) return null;
 
   return (
     <header className="mobile:flex laptop:hidden h-14 shadow items-center justify-between dark:border-b bg-white dark:bg-gray-800">
-      <div className="px-9">Yu-Won</div>
-        <button
-          className="flex px-9 cursor-pointer"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-        >
-          {
-            theme === "dark" ?
-              <MoonIcon className="w-6 h-6" />
-              : <SunIcon className="w-6 h-6" />
-          }
-        </button>
+      <Lottie className="px-9 h-20" animationData={cat} loop />
+      <button
+        className="flex px-9 cursor-pointer"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        {theme === "dark" ? (
+          <MoonIcon className="w-6 h-6" />
+        ) : (
+          <SunIcon className="w-6 h-6" />
+        )}
+      </button>
     </header>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,11 @@
+import Lottie from "lottie-react";
+import cat from "../public/json/indigo-cat.json";
 import Gnb from "./Gnb";
 
 const Sidebar = () => {
   return (
     <aside className="mobile:hidden laptop:flex w-20 h-screen flex-col text-center justify-evenly">
-      <div>Yu-Won</div>
+      <Lottie animationData={cat} loop />
       <Gnb />
     </aside>
   );

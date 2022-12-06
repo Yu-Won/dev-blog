9f5ed7a1756655fe05c24748caa86b62c1a0a905
YU-WON <shallwedance0419@gmail.com>
2022-12-06 20:12:34 +0900

    remove: comment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-// const REGEX_HTTP_PROTOCOL_PREFIX = new RegExp("^(https?://)", "i");
-
-// const getImageDomainHost = (domain) => {
-//   if (!domain) {
-//     return "";
-//   }
-//   return domain.replace(REGEX_HTTP_PROTOCOL_PREFIX, "");
-// };
-
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
@@ -21,12 +12,6 @@ module.exports = {
   trailingSlash: false,
   images: {
     formats: ["image/avif", "image/webp"],
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: getImageDomainHost(process.env.NEXT_PUBLIC_CDN_IMG_URL),
-    //   },
-    // ],
     imageSizes: [64, 256],
     deviceSizes: [640],
   },

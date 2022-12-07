76009d4a582fd2324228faebabacc617e064c9f2
YU-WON <shallwedance0419@gmail.com>
2022-12-07 19:13:53 +0900

    modify: code text style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,51 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   darkMode: "class",
   content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     fontFamily: {
-      sans: ["Roboto", "sans-serif"]
+      sans: ["Roboto", "sans-serif"],
     },
     extend: {
       screens: {
         mobile: "320px",
         tablet: "768px",
         laptop: "1024px",
-        desktop: "1440px"
+        desktop: "1440px",
       },
       typography: (theme) => ({
-        dark: {
+        DEFAULT: {
           css: {
-            color: theme('colors.gray.300'),
-            h1: {
-              color: theme('colors.gray.100'),
+            "code::before": {
+              content: '""',
             },
-            h2: {
-              color: theme('colors.gray.100'),
+            "code::after": {
+              content: '""',
             },
           },
-        }
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            h1: {
+              color: theme("colors.gray.100"),
+            },
+            h2: {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
       }),
-    }
+    },
   },
   variants: {
     extend: {
-      typography: ['dark'],
-    }
+      typography: ["dark"],
+    },
   },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};

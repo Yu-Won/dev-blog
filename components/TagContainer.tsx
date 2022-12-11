0577092e607a0c71d7dd88b849b416057294d3b7
YU-WON <shallwedance0419@gmail.com>
2022-12-11 20:28:15 +0900

    modify: style

--- components/TagContainer.tsx
+++ components/TagContainer.tsx
@@ -2,27 +2,23 @@ import Link from "next/link";
 import { TagWithCount } from "types";
 
 interface IProps {
-    tags: TagWithCount[];
+  tags: TagWithCount[];
 }
 
 const TagContainer = (props: IProps) => {
   const { tags } = props;
   return (
-    <div className="flex mx-auto w-9/12 mobile:pt-12 tablet:pt-24 border-b dark:border-zinc-500">
-      <ul className="flex w-full overflow-x-scroll whitespace-nowrap float-left space-x-6">
+    <div className="mx-auto w-9/12 mobile:pt-12 tablet:pt-32 border-b dark:border-zinc-500">
+      <ul className="flex absolute mobile:top-16 tablet:top-36 laptop:top-20 overflow-auto h-12 mobile:w-3/4 tablet:w-[76%] laptop:w-[56%] desktop:w-[60%] space-x-6 items-center">
         {tags.map((tagValue) => (
           <li
             key={tagValue.tag}
-            className="flex items-center relative whitespace-nowrap py-1.5 space-x-2"
+            className="w-fit items-center whitespace-nowrap py-1.5 space-x-2"
           >
-            <Link href={`/tag/${tagValue.tag}`}>
-              {tagValue.tag}
-            </Link>
-            <div
-              className="text-sm text-violet-200 dark:text-indigo-500"
-            >
+            <Link href={`/tag/${tagValue.tag}`}>{tagValue.tag}</Link>
+            <span className="text-sm text-violet-200 dark:text-indigo-500">
               {tagValue.count}
-            </div>
+            </span>
           </li>
         ))}
       </ul>

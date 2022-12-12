b49e5920215ba86b2fd6f560a354f35b2631d68a
YU-WON <shallwedance0419@gmail.com>
2022-12-12 14:14:18 +0900

    modify: style

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,9 +1,7 @@
 const Title = (props: { id: string }) => {
   return (
     <div className="flex mx-auto w-9/12 mobile:pt-12 tablet:pt-24 border-b dark:border-zinc-500">
-      <span className="font-bold text-3xl">
-         {props.id}
-      </span>
+      <span className="font-bold text-3xl py-2">{props.id}</span>
     </div>
   );
 };

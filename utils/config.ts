7a00383c39c600858c3e494f5170a6b1b4c5711a
YU-WON <shallwedance0419@gmail.com>
2022-12-19 20:36:53 +0900

    modify: config

--- utils/config.ts
+++ utils/config.ts
@@ -1,25 +1,25 @@
 export const SiteConfig = {
-    url: "https://Yu-Won.xyz",
-    pathPrefix: "/",
-    title: "Yu-Won dev-blog",
-    subtitle: "dev-blog",
-    copyright: "Yu-Won © All rights reserved.",
-    disqusShortname: "",
-    googleAnalyticsId: "",
-    menu: [
-      {
-        label: "Tags",
-        path: "/tags"
-      }
-    ],
-    author: {
-      name: "Yu-Won",
-      photo: "/public/images/profile.jpeg",
-      bio: "frontend engineer",
-      contacts: {
-        email: "shallwedance0419@gmail.com",
-        github: "https://github.com/Yu-Won",
-        linkedin: ""
-      }
-    }
+  url: "https://Yu-Won.xyz",
+  pathPrefix: "/",
+  title: "Yu-Won dev-blog",
+  subtitle: "dev-blog",
+  copyright: "Yu-Won © All rights reserved.",
+  disqusShortname: "",
+  googleAnalyticsId: "G-8VWJM19CSZ",
+  menu: [
+    {
+      label: "Tags",
+      path: "/tags",
+    },
+  ],
+  author: {
+    name: "Yu-Won",
+    photo: "/public/images/profile.jpeg",
+    bio: "frontend engineer",
+    contacts: {
+      email: "shallwedance0419@gmail.com",
+      github: "https://github.com/Yu-Won",
+      linkedin: "https://www.linkedin.com/in/yu-won",
+    },
+  },
 };

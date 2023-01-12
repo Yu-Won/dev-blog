f60760c91771a1fe0cc928724246e67ed69a6a04
YU-WON <shallwedance0419@gmail.com>
2023-01-12 21:52:44 +0900

    modify: prose style

--- pages/post/[year]/[...slugs].tsx
+++ pages/post/[year]/[...slugs].tsx
@@ -7,79 +7,79 @@ import { Post } from "types";
 import { SiteConfig } from "utils/config";
 
 interface SlugInterface {
-  [key: string]: string | string[] | undefined;
-  year: string;
-  slugs: string[];
+	[key: string]: string | string[] | undefined;
+	year: string;
+	slugs: string[];
 }
 
 const PostPage = ({
-  post,
-  mdx,
-  slug,
+	post,
+	mdx,
+	slug,
 }: {
-  post: Post;
-  mdx: MDXRemoteSerializeResult;
-  slug: string;
+	post: Post;
+	mdx: MDXRemoteSerializeResult;
+	slug: string;
 }) => {
-  const { title, tags, date, description } = post.frontMatter;
-  return (
-    <div className="pt-16 pb-24 px-4 mx-auto prose dark:prose-invert mobile:prose-sm tablet:prose-base prose-h1:text-center">
-      <Head>
-        <meta name="title" content={title} />
-        {tags.map((tag) => (
-          <meta key={tag} name="keywords" content={tag} />
-        ))}
-        <meta name="description" content={description} />
-      </Head>
-      <PostSeo
-        title={title}
-        tags={tags}
-        date={date}
-        description={description}
-        url={`${SiteConfig.url}/${slug}`}
-      />
-      <MDXRemote {...mdx} />
-    </div>
-  );
+	const { title, tags, date, description } = post.frontMatter;
+	return (
+		<div className="pt-16 pb-24 px-4 mx-auto prose dark:prose-invert mobile:prose-sm tablet:prose-base prose-h1:text-center prose-blockquote:not-italic prose-blockquote:font-normal">
+			<Head>
+				<meta name="title" content={title} />
+				{tags.map((tag) => (
+					<meta key={tag} name="keywords" content={tag} />
+				))}
+				<meta name="description" content={description} />
+			</Head>
+			<PostSeo
+				title={title}
+				tags={tags}
+				date={date}
+				description={description}
+				url={`${SiteConfig.url}/${slug}`}
+			/>
+			<MDXRemote {...mdx} />
+		</div>
+	);
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const allPosts = await getAllPosts();
-  const paths: { params: { year: string; slugs: string[] } }[] =
-    allPosts.reduce<{ params: { year: string; slugs: string[] } }[]>(
-      (prev, { fields: { slug } }) => {
-        const [year, ...slugs] = `${slug.replace(".md", "")}`.split("/");
-        prev.push({ params: { year, slugs } });
-        return prev;
-      },
-      []
-    );
+	const allPosts = await getAllPosts();
+	const paths: { params: { year: string; slugs: string[] } }[] =
+		allPosts.reduce<{ params: { year: string; slugs: string[] } }[]>(
+			(prev, { fields: { slug } }) => {
+				const [year, ...slugs] = `${slug.replace(".md", "")}`.split("/");
+				prev.push({ params: { year, slugs } });
+				return prev;
+			},
+			[],
+		);
 
-  return {
-    paths,
-    fallback: false,
-  };
+	return {
+		paths,
+		fallback: false,
+	};
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { year, slugs } = params as SlugInterface;
+	const { year, slugs } = params as SlugInterface;
 
-  const slug = [year, ...(slugs as string[])].join("/");
-  const posts = await getAllPosts();
-  const post = posts.find((p) => p?.fields?.slug === slug);
-  if (post) {
-    const source = await parseMarkdownToMdx(post.body, post.path);
-    return {
-      props: {
-        post,
-        mdx: source,
-        slug,
-      },
-    };
-  }
-  return {
-    notFound: true,
-  };
+	const slug = [year, ...(slugs as string[])].join("/");
+	const posts = await getAllPosts();
+	const post = posts.find((p) => p?.fields?.slug === slug);
+	if (post) {
+		const source = await parseMarkdownToMdx(post.body, post.path);
+		return {
+			props: {
+				post,
+				mdx: source,
+				slug,
+			},
+		};
+	}
+	return {
+		notFound: true,
+	};
 };
 
 export default PostPage;

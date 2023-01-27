e5b20495b2cf22d7cc36ac4ea44d03d958c5189f
YU-WON <shallwedance0419@gmail.com>
2023-01-28 00:14:20 +0900

    modify: eslint error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,55 +14,72 @@ import { defaultImagePlaceHolder, SiteConfig } from "utils/config";
 import { pageview } from "utils/gtag";
 
 const ResponsiveImage = (props: any) => (
-  <Image
-    alt={props.alt}
-    layout="responsive"
-    className="w-full h-full"
-    priority
-    placeholder={defaultImagePlaceHolder}
-    {...props}
-  />
+	<Image
+		alt={props.alt}
+		layout="responsive"
+		className="w-full h-full"
+		priority
+		placeholder={defaultImagePlaceHolder}
+		{...props}
+	/>
 );
 
 const components = {
-  img: ResponsiveImage,
+	img: ResponsiveImage,
 };
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+	getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+	Component: NextPageWithLayout;
 };
 
-const yuWonDevBlog = ({ Component, pageProps }: AppPropsWithLayout) => {
-  const { events } = useRouter();
-  useEffect(() => {
-    const handelRouteChange = (url: string) => {
-      pageview(url);
-    };
+const YuWonDevBlog = ({ Component, pageProps }: AppPropsWithLayout) => {
+	const { events } = useRouter();
+	useEffect(() => {
+		const handelRouteChange = (url: string) => {
+			pageview(url);
+		};
 
-    events.on("routeChangeComplete", handelRouteChange);
-    events.on("hashChangeComplete", handelRouteChange);
+		events.on("routeChangeComplete", handelRouteChange);
+		events.on("hashChangeComplete", handelRouteChange);
 
-    return () => {
-      events.off("routeChangeComplete", handelRouteChange);
-      events.off("hashChangeComplete", handelRouteChange);
-    };
-  }, [events]);
+		return () => {
+			events.off("routeChangeComplete", handelRouteChange);
+			events.off("hashChangeComplete", handelRouteChange);
+			console.log(`
+       ,-.       _,---._ __  / \\
+      /  )    .-'       \`./ /   \\
+     (  (   ,'            \`/    /|
+      \\  \`-"             \\'\\   / |
+       \`.              ,  \\ \\ /  |
+        /\`.          ,'-\`----Y   |
+       (            ;  R̲e̲a̲c̲t̲ |   '
+       |  ,-.    ,-'      T̲S̲ |  /
+       |  | (   |            | /
+       )  |  \\  \`.___________|/
+       \`--'   \`--'`);
+			console.log(
+				"%cฅ^•ﻌ•^ฅ About me:",
+				"color: #ffffff;",
+				"https://www.yu-won.blog/about",
+			);
+		};
+	}, [events]);
 
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${SiteConfig.googleAnalyticsId}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${SiteConfig.googleAnalyticsId}`}
+			/>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -70,18 +87,18 @@ const yuWonDevBlog = ({ Component, pageProps }: AppPropsWithLayout) => {
               page_path: window.location.pathname,
             });
           `,
-        }}
-      />
-      <ThemeProvider attribute="class">
-        <DefaultSeo {...SEO} />
-        <MDXProvider components={components}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </MDXProvider>
-      </ThemeProvider>
-    </>
-  );
+				}}
+			/>
+			<ThemeProvider attribute="class">
+				<DefaultSeo {...SEO} />
+				<MDXProvider components={components}>
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				</MDXProvider>
+			</ThemeProvider>
+		</>
+	);
 };
 
-export default yuWonDevBlog;
+export default YuWonDevBlog;

63afd4ae81717002aefb78db60eabdfddf208f09
YU-WON <shallwedance0419@gmail.com>
2023-03-28 21:43:10 +0900

    modify: redirect function

--- next.config.js
+++ next.config.js
@@ -1,41 +1,42 @@
 /** @type {import('next').NextConfig} */
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+	enabled: process.env.ANALYZE === "true",
 });
 
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
-  swcMinify: true,
-  productionBrowserSourceMaps: false,
-  compress: true,
-  poweredByHeader: false,
-  httpAgentOptions: {
-    keepAlive: false,
-  },
-  trailingSlash: false,
-  images: {
-    formats: ["image/avif", "image/webp"],
-    imageSizes: [64, 256],
-    deviceSizes: [640],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/1",
-      },
-    ];
-  },
-  webpack: (config, options) => {
-    config.plugins.push(new CompressionPlugin());
+	reactStrictMode: true,
+	swcMinify: true,
+	productionBrowserSourceMaps: false,
+	compress: true,
+	poweredByHeader: false,
+	httpAgentOptions: {
+		keepAlive: false,
+	},
+	trailingSlash: false,
+	images: {
+		formats: ["image/avif", "image/webp"],
+		imageSizes: [64, 256],
+		deviceSizes: [640],
+	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/1",
+				permanent: false,
+			},
+		];
+	},
+	webpack: (config, options) => {
+		config.plugins.push(new CompressionPlugin());
 
-    return {
-      ...config,
-      mode: options.dev ? "development" : "production",
-      devtool: options.dev ? "eval-source-map" : "hidden-source-map",
-    };
-  },
+		return {
+			...config,
+			mode: options.dev ? "development" : "production",
+			devtool: options.dev ? "eval-source-map" : "hidden-source-map",
+		};
+	},
 });
